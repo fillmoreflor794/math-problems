09fec0b82a3616ed45eec0e53ca7ec603f236e7f
fillmoreflor794 <fillmoreflor794@gmail.com>
2025-04-08 16:52:21 +0000

    Add 28.rs

---
+++ src/28.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // Example code 1: Addition
+    let num1 = 5;
+    let num2 = 3;
+    println!("The sum of {} and {} is {}", num1, num2, num1 + num2);
+
+    // Example code 2: Subtraction
+    let num3 = 8;
+    let num4 = 2;
+    println!("The difference between {} and {} is {}", num3, num4, num3 - num4);
+
+    // Example code 3: Multiplication
+    let num5 = 7;
+    let num6 = 2;
+    println!("The product of {} and {} is {}", num5, num6, num5 * num6);
+}
