68e187e0f2b3e48822a924daa94fdfd78290e810
fillmoreflor794 <fillmoreflor794@gmail.com>
2025-04-23 10:48:57 +0000

    Add 30.rs

---
+++ src/30.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Example code snippet with basic operations and conditions
+    let num1 = 5;
+    let num2 = 3;
+
+    if num1 > num2 {
+        println!("num1 is greater than num2");
+    } else if num1 < num2 {
+        println!("num1 is less than num2");
+    } else {
+        println!("Both numbers are equal");
+    }
+}
