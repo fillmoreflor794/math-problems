755f2b2419d01feff4a13beffefeabf4eb2f6b2b
fillmoreflor794 <fillmoreflor794@gmail.com>
2025-05-24 02:15:52 +0000

    Add 39.rs

---
+++ src/39.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // Exercise 1: Addition and Subtraction
+    let num1 = 5;
+    let num2 = 3;
+    println!("The sum of {} and {} is {}", num1, num2, num1 + num2);
+    
+    // Exercise 2: Multiplication
+    let num3 = 4;
+    let num4 = 2;
+    println!("The product of {} and {} is {}", num3, num4, num3 * num4);
+    
+    // Exercise 3: Floor Division
+    let num5 = 10.7;
+    let num6 = 3;
+    println!("The quotient of {} divided by {} is {:.2}", num5, num6, num5 / num6);
+    
+    // Exercise 4: Integer Division
+    let num7 = 8;
+    let num8 = 2;
+    println!("The integer division of {} and {} is {}", num7, num8, num7 / num8);
+}
