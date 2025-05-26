faf96c228e3fc905b6bba21704d8ff6491e35bb9
fillmoreflor794 <fillmoreflor794@gmail.com>
2025-05-26 05:29:48 +0000

    Add 41.rs

---
+++ src/41.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // Example Rust code for practicing mathematical operations
+
+    let a = 5;
+    let b = 3;
+
+    // Addition
+    let sum = a + b;
+
+    // Subtraction
+    let diff = a - b;
+
+    // Multiplication
+    let product = a * b;
+
+    // Division
+    let quotient = a / b;
+
+    // Modulus (remainder)
+    let remainder = a % b;
+
+    println!("The result of addition is: {}", sum);
+    println!("The result of subtraction is: {}", diff);
+    println!("The result of multiplication is: {}", product);
+    println!("The result of division is: {}", quotient);
+    println!("The result of modulus (remainder) is: {}", remainder);
+}
